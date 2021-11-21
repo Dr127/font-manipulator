5dc2423f4da3f6aa2c92dbf1758bd99ef1f286c0
Dr127 <79019262+Dr127@users.noreply.github.com>
2021-11-21 18:41:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+noseX = 0;
+noseY = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500)
@@ -11,12 +16,25 @@ function setup(){
 function modelLoaded(){
     console.log("PoseNet is Initialized");
 }
-function draw(){
-    background('#969A7');
-}
+
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log(noseX = results.pose.nose.x);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" + noseX + "noseY =" + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX + "rightWristX =" +rightWristX + "difference =" + difference);
     }
+}
+
+function draw(){
+    background('#969A7');
+
+    textSize(difference);
+    fill('#FFE787');
+    text('Peter', noseX, noseY,);
 }
